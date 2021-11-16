c2d4b2522f242550f7d6780d4d112b8361ca5518
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-16 22:59:32 +0530

    ADDED : the main fuction giving three values

---
+++ src/com/LinkedList/ext/Main.java
@@ -0,0 +1,14 @@
+package com.LinkedList.ext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+
+		list.print();
+	}
+
+}
