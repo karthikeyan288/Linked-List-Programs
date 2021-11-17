462bdc28d79140c6c77eebae5866b2d4e39c2cc9
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-17 07:42:11 +0530

    ADDED : deleting the last value

---
+++ src/com/LinkedList/ext/Main.java
@@ -0,0 +1,16 @@
+package com.LinkedList.ext;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(70);
+		list.insert(1, 30);
+		list.poplast();
+
+		list.print();
+  }
+
+}
