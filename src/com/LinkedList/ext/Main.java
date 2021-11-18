4c2d333bd9a947a18e9ae41ed38b6dece0620c09
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-18 22:05:59 +0530

    ADDED : the main fuction

---
+++ src/com/LinkedList/ext/Main.java
@@ -0,0 +1,18 @@
+package com.LinkedList.ext;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(70);
+		list.insert(1, 30);
+		list.poplast();
+		
+		list.Search(56);
+
+		list.print();
+  }
+
+}
