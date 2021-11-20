ab90c37481c5207aa4977389e32f10857fe28c15
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-20 17:34:46 +0530

    ADDED : inserting 40 after 30

---
+++ src/com/LinkedList/ext/Main.java
@@ -0,0 +1,17 @@
+package com.LinkedList.ext;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		LinkedList list = new LinkedList();
+		list.add(56);
+		list.add(70);
+		list.insert(1, 30);
+		list.insert(2, 40);
+ 
+		list.print();
+  }
+
+
+}
